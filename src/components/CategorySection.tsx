4a3dc373f2b171a5964482c15143dd6989b6f7e7
JakubPaszko2001 <jakubpaszko01@gmail.com>
2023-04-16 19:17:47 +0200

    feat: :sparkles: Add swiper

--- src/components/CategorySection.tsx
+++ src/components/CategorySection.tsx
@@ -4,6 +4,12 @@ import fetchCategory from "../config/axios";
 const CategorySection = () => {
   const [category, setCategory] = useState([]);
 
+  interface Category {
+    idCategory: string;
+    strCategoryThumb: string;
+    strCategory: string;
+  }
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -18,12 +24,11 @@ const CategorySection = () => {
 
   return (
     <section className="w-full flex flex-col justify-center items-center gap-4">
-      <h2 className="text-4xl">Food Category</h2>
+      <h2 className="text-4xl">Food Categorys</h2>
       {category.length > 0 ? (
-        // Render the category data if it exists
-        category.map((item) => (
+        category.slice(0, 12).map((item: Category) => (
           <div
-            key={item.idCategory} // Assuming item has an id property
+            key={item.idCategory}
             className="w-2/3 flex flex-col justify-center items-center p-4 bg-mainGreen rounded-lg cursor-pointer"
           >
             <img src={item.strCategoryThumb} />
