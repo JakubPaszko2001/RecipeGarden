a537c8e8c585c9d83b56e9eb2dce76eb64981cdd
JakubPaszko2001 <jakubpaszko01@gmail.com>
2023-04-16 21:16:41 +0200

    feat :sparkles: Add Fetch popular recipe

--- src/config/axios.tsx
+++ src/config/axios.tsx
@@ -22,4 +22,26 @@ const fetchCategory = () => {
     });
 };
 
-export default fetchCategory;
+const fetchPopular = () => {
+  const options = {
+    method: "GET",
+    url: "https://themealdb.p.rapidapi.com/latest.php",
+    headers: {
+      "X-RapidAPI-Key": "57607c4060msh8305edef8d177d9p1a088fjsnad865a1577b2",
+      "X-RapidAPI-Host": "themealdb.p.rapidapi.com",
+    },
+  };
+
+  return axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data.meals);
+      return response.data.meals;
+    })
+    .catch(function (error) {
+      console.error(error);
+      throw error;
+    });
+};
+
+export { fetchCategory, fetchPopular };
