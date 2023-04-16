6d3ee361df33681f24fb6c9287324b461abab581
JakubPaszko2001 <jakubpaszko01@gmail.com>
2023-04-16 21:17:12 +0200

    style :lipstick: Add z-index

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   const [hamburgerMenu, setHamburgerMenu] = React.useState<boolean>(false);
 
   return (
-    <nav className="h-[10vh] w-full fixed top-0 left-0 flex justify-between items-center p-4 bg-white">
+    <nav className="h-[10vh] w-full fixed top-0 left-0 flex justify-between items-center p-4 bg-white z-50">
       <h1 className="text-3xl">
         Recipe<span className="text-mainGreen">Garden</span>
       </h1>
