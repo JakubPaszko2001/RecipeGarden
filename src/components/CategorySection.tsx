a31e97ffd1a01c9445e3616a41eb9c924017f731
JakubPaszko2001 <jakubpaszko01@gmail.com>
2023-04-16 21:19:33 +0200

    fix :art: Delete some unnecessary code

--- src/components/CategorySection.tsx
+++ src/components/CategorySection.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
-import fetchCategory from "../config/axios";
+import { fetchCategory } from "../config/axios";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 
 const CategorySection = () => {
   const [category, setCategory] = useState([]);
+  const [popular, setPopular] = useState([]);
 
   interface Category {
     idCategory: string;
@@ -19,25 +23,31 @@ const CategorySection = () => {
         console.error(error);
       }
     };
-    fetchData(); // Call the fetchData function
+    fetchData();
   }, []);
 
   return (
     <section className="w-full flex flex-col justify-center items-center gap-4">
       <h2 className="text-4xl">Food Categorys</h2>
-      {category.length > 0 ? (
-        category.slice(0, 12).map((item: Category) => (
-          <div
-            key={item.idCategory}
-            className="w-2/3 flex flex-col justify-center items-center p-4 bg-mainGreen rounded-lg cursor-pointer"
-          >
-            <img src={item.strCategoryThumb} />
-            <p className="text-3xl text-white">{item.strCategory}</p>
-          </div>
-        ))
-      ) : (
-        <p>Loading...</p>
-      )}
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        className="mySwiper"
+      >
+        {category.length > 0 ? (
+          category.slice(0, 12).map((item: Category) => (
+            <SwiperSlide key={item.idCategory}>
+              <img src={item.strCategoryThumb} />
+              <p className="text-xl">{item.strCategory}</p>
+            </SwiperSlide>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </Swiper>
     </section>
   );
 };
