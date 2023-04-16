99a118be3b421cc548e869878178cce6a6a3d390
JakubPaszko2001 <jakubpaszko01@gmail.com>
2023-04-16 14:26:05 +0200

    feat: :sparkles: Add Api test

---
+++ cypress/component/ApiTest.cy.js
@@ -0,0 +1,17 @@
+describe("ApiTest.cy.tsx", () => {
+  it("GET", () => {
+    cy.request({
+      method: "GET",
+      url: "https://themealdb.p.rapidapi.com/categories.php",
+      headers: {
+        "X-RapidAPI-Key": "57607c4060msh8305edef8d177d9p1a088fjsnad865a1577b2",
+        "X-RapidAPI-Host": "themealdb.p.rapidapi.com",
+      },
+    })
+      .its("body")
+      .its("categories")
+      .should("have.length", 14);
+    // .its("status")
+    // .should("equal", 200);
+  });
+});
