c7932bb5dc77d981162d18411bdb5410e2b27bbd
JakubPaszko2001 <jakubpaszko01@gmail.com>
2023-04-17 21:33:30 +0200

    test: :sparkles: Add status check

--- cypress/component/ApiTest.cy.js
+++ cypress/component/ApiTest.cy.js
@@ -1,5 +1,5 @@
 describe("ApiTest.cy.tsx", () => {
-  it("GET", () => {
+  it("Status chceck", () => {
     cy.request({
       method: "GET",
       url: "https://themealdb.p.rapidapi.com/categories.php",
@@ -9,7 +9,17 @@ describe("ApiTest.cy.tsx", () => {
       },
     })
       .its("status")
-      .should("equal", 200)
+      .should("equal", 200);
+  });
+  it("Array check", () => {
+    cy.request({
+      method: "GET",
+      url: "https://themealdb.p.rapidapi.com/categories.php",
+      headers: {
+        "X-RapidAPI-Key": "57607c4060msh8305edef8d177d9p1a088fjsnad865a1577b2",
+        "X-RapidAPI-Host": "themealdb.p.rapidapi.com",
+      },
+    })
       .its("body")
       .its("categories")
       .should("have.length", 14);
