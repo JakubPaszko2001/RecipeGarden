381a1111f63e471c5c5a590e48720ee2c36cc284
JakubPaszko2001 <jakubpaszko01@gmail.com>
2023-04-24 21:34:57 +0200

    refactor :art: Add Section to the name

---
+++ src/components/PopularRecipeSection.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { fetchPopular } from "../config/axios";
+
+const PopularRecipe = () => {
+  const [popular, setPopular] = useState([]);
+
+  interface Popular {
+    idMeal: string;
+    strMealThumb: string;
+    strYoutube: string;
+  }
+
+  useEffect(() => {
+    const fetchPopularRecipe = async () => {
+      try {
+        const response = await fetchPopular();
+        setPopular(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchPopularRecipe();
+  }, []);
+  return (
+    <section>
+      <h2 className="text-center text-4xl mb-4">Popular Food</h2>
+      <div className="w-full grid grid-cols-2 gap-4 p-4">
+        {popular.length > 0 ? (
+          popular.map((item: Popular) => (
+            <div className="flex flex-col gap-2" key={item.idMeal}>
+              <img className="rounded-xl" src={item.strMealThumb} />
+              <button className="w-full border-2 border-mainGreen rounded-xl">
+                Details
+              </button>
+              <a
+                href={item.strYoutube}
+                target="_blank"
+                className="w-full text-center border-2 border-mainGreen rounded-xl"
+              >
+                Video
+              </a>
+            </div>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default PopularRecipe;
