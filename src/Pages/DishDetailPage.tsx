2d808b2ba791d6a0176fe352d99dddc9dc421dae
JakubPaszko2001 <jakubpaszko01@gmail.com>
2023-04-25 20:38:02 +0200

    feat :sparkles: Add DishDetailPage

--- src/Pages/DishDetailPage.tsx
+++ src/Pages/DishDetailPage.tsx
@@ -1,7 +1,37 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { dishFetch } from "../config/axios";
 const DishDetailPage = () => {
-  return <div>DishDetailPage</div>;
+  const [dishData, setDishData] = useState([]);
+  const { dishId } = useParams();
+  console.log(dishId);
+
+  interface Dish {
+    strMealThumb: string;
+  }
+
+  useEffect(() => {
+    const dishFetchData = async () => {
+      try {
+        const response = await dishFetch(dishId);
+        // console.log(response);
+        setDishData(response);
+        console.log(dishData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    dishFetchData();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        {dishData &&
+          dishData.map((dish: Dish) => <img src={dish.strMealThumb} />)}
+      </div>
+    </div>
+  );
 };
 
 export default DishDetailPage;
