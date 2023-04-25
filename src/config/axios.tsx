377fec52fa1c08f87cbaf3482df35e1cb9d8ebab
JakubPaszko2001 <jakubpaszko01@gmail.com>
2023-04-25 20:36:09 +0200

    feat :sparkles: Add dishFetch

--- src/config/axios.tsx
+++ src/config/axios.tsx
@@ -65,5 +65,27 @@ const categoryFetch = (category: string | undefined) => {
       console.error(error);
     });
 };
+const dishFetch = (dishId: string | undefined) => {
+  const options = {
+    method: "GET",
+    url: "https://themealdb.p.rapidapi.com/lookup.php",
+    params: { i: dishId },
+    headers: {
+      "content-type": "application/octet-stream",
+      "X-RapidAPI-Key": "57607c4060msh8305edef8d177d9p1a088fjsnad865a1577b2",
+      "X-RapidAPI-Host": "themealdb.p.rapidapi.com",
+    },
+  };
 
-export { fetchCategory, fetchPopular, categoryFetch };
+  return axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data.meals);
+      return response.data.meals;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+};
+
+export { fetchCategory, fetchPopular, categoryFetch, dishFetch };
