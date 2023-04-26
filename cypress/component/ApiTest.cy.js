e7c65e85362022f1d5eeb98fc399adf1ddd6d0e7
JakubPaszko2001 <jakubpaszko01@gmail.com>
2023-04-26 22:34:16 +0200

    test :sparkles: Change Description | Add popular food and Specific dish test's

--- cypress/component/ApiTest.cy.js
+++ cypress/component/ApiTest.cy.js
@@ -1,4 +1,4 @@
-describe("ApiTest.cy.tsx", () => {
+describe("Food Category Api Test", () => {
   it("Status chceck", () => {
     cy.request({
       method: "GET",
@@ -11,7 +11,7 @@ describe("ApiTest.cy.tsx", () => {
       .its("status")
       .should("equal", 200);
   });
-  it("Array check", () => {
+  it("Food Category Array Test", () => {
     cy.request({
       method: "GET",
       url: "https://themealdb.p.rapidapi.com/categories.php",
@@ -23,5 +23,53 @@ describe("ApiTest.cy.tsx", () => {
       .its("body")
       .its("categories")
       .should("have.length", 14);
+  });
+});
+
+describe("Popular Food Test", () => {
+  it("Status chceck", () => {
+    cy.request({
+      method: "GET",
+      url: "https://themealdb.p.rapidapi.com/latest.php",
+      headers: {
+        "X-RapidAPI-Key": "57607c4060msh8305edef8d177d9p1a088fjsnad865a1577b2",
+        "X-RapidAPI-Host": "themealdb.p.rapidapi.com",
+      },
+    })
+      .its("status")
+      .should("equal", 200);
+  });
+});
+
+describe("Specific Category Test", () => {
+  it("Status chceck", () => {
+    cy.request({
+      method: "GET",
+      url: "https://themealdb.p.rapidapi.com/filter.php",
+      params: { c: "Beef" },
+      headers: {
+        "X-RapidAPI-Key": "57607c4060msh8305edef8d177d9p1a088fjsnad865a1577b2",
+        "X-RapidAPI-Host": "themealdb.p.rapidapi.com",
+      },
+    })
+      .its("status")
+      .should("equal", 200);
+  });
+});
+
+describe("Specific Dish Test", () => {
+  it("Status chceck", () => {
+    cy.request({
+      method: "GET",
+      url: "https://themealdb.p.rapidapi.com/lookup.php",
+      params: { i: "53065" },
+      headers: {
+        "content-type": "application/octet-stream",
+        "X-RapidAPI-Key": "57607c4060msh8305edef8d177d9p1a088fjsnad865a1577b2",
+        "X-RapidAPI-Host": "themealdb.p.rapidapi.com",
+      },
+    })
+      .its("status")
+      .should("equal", 200);
   });
 });
