993221b99a3e053715dcdc0d22b269a74b72b9b1
JakubPaszko2001 <jakubpaszko01@gmail.com>
2023-04-30 12:39:55 +0200

    feat :sparkles: Add navigation to Home component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
 import HamburgerMenu from "./HamburgerMenu";
 import MobileNav from "./MobileNav";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const [hamburgerMenu, setHamburgerMenu] = React.useState<boolean>(false);
+  const navigate = useNavigate();
+
+  const navigateToHome = () => {
+    navigate(`/`);
+  };
 
   return (
     <nav className="h-[10vh] w-full fixed top-0 left-0 flex justify-between items-center p-4 bg-white z-50">
-      <h1 className="text-3xl">
+      <h1
+        onClick={() => {
+          navigateToHome();
+        }}
+        className="text-3xl cursor-pointer"
+      >
         Recipe<span className="text-mainGreen">Garden</span>
       </h1>
       <MobileNav setHamburgerMenu={setHamburgerMenu} />
