bcea075e399f81a2443675cf7fae09ebc6da78d0
JakubPaszko2001 <jakubpaszko01@gmail.com>
2023-05-18 00:39:18 +0200

    feat :sparkles: Add display all dishes from database

--- src/Pages/ShoppingListPage.tsx
+++ src/Pages/ShoppingListPage.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../config/firebase";
 
-const ShoppingList = () => {
-  return <div>ShoppingList</div>;
+const ShoppingList = ({ currentUser }: { currentUser: any }) => {
+  const [shoppingList, setShoppingList] = useState<string[]>([]);
+
+  useEffect(() => {
+    getAllDocuments();
+  }, []);
+
+  const collectionRef = collection(db, "Ingredients", currentUser.uid, "Dish");
+
+  const getAllDocuments = async () => {
+    const querySnapshot = await getDocs(collectionRef);
+    const items: string[] = [];
+
+    querySnapshot.forEach((doc) => {
+      items.push(doc.id);
+    });
+
+    setShoppingList(items);
+  };
+
+  console.log(shoppingList);
+
+  return (
+    <div>
+      <Navbar />
+      <div className="mt-[10vh]">
+        {shoppingList && shoppingList.map((item) => <p key={item}>{item}</p>)}
+      </div>
+    </div>
+  );
 };
 
 export default ShoppingList;
