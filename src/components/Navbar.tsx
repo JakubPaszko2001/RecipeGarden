d97f5a337b6351e270905d30f9704e8f08c2f4f9
JakubPaszko2001 <jakubpaszko01@gmail.com>
2023-05-30 23:08:18 +0200

    fear :sparkles: Add SearchBar and pass props

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import HamburgerMenu from "./HamburgerMenu";
 import MobileNav from "./MobileNav";
 import { useNavigate } from "react-router-dom";
+import SearchBar from "./SearchBar";
 
 const Navbar = () => {
   const [hamburgerMenu, setHamburgerMenu] = React.useState<boolean>(false);
+  const [searchBar, setSearchBar] = React.useState<boolean>(false);
   const navigate = useNavigate();
 
   const navigateToHome = () => {
@@ -21,11 +23,15 @@ const Navbar = () => {
       >
         Recipe<span className="text-mainGreen">Garden</span>
       </h1>
-      <MobileNav setHamburgerMenu={setHamburgerMenu} />
+      <MobileNav
+        setHamburgerMenu={setHamburgerMenu}
+        setSearchBar={setSearchBar}
+      />
       <HamburgerMenu
         hamburgerMenu={hamburgerMenu}
         setHamburgerMenu={setHamburgerMenu}
       />
+      <SearchBar searchBar={searchBar} setSearchBar={setSearchBar} />
     </nav>
   );
 };
