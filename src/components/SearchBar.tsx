c3bbd3dc554140890383327cdd1433aa94b7247d
JakubPaszko2001 <jakubpaszko01@gmail.com>
2023-05-31 22:49:12 +0200

    feat :sparkles: Add SearchBy and Input to search

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { AiOutlineClose } from "react-icons/ai";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineSearch } from "react-icons/ai";
 
 interface Props {
   searchBar: boolean;
@@ -8,10 +7,22 @@ interface Props {
 }
 
 const SearchBar = ({ searchBar, setSearchBar }: Props) => {
-  const navigate = useNavigate();
-  const navigateToShoppingList = () => {
-    navigate(`/shoppinglist`);
-  };
+  const [searchBy, setSearchBy] = useState("Ingredient");
+
+  function changeSearchBy(value: string) {
+    setSearchBy(value);
+  }
+
+  function getInputPlaceholder() {
+    if (searchBy === "Ingredient") {
+      return "Type Ingredient (garlic)";
+    } else if (searchBy === "Multi-Ingredient") {
+      return "Type Multiple Ingredients (garlic,salt)";
+    } else if (searchBy === "Dish Name") {
+      return "Type Dish Name (Arrabiata)";
+    }
+    return "";
+  }
 
   return (
     <nav
@@ -26,6 +37,43 @@ const SearchBar = ({ searchBar, setSearchBar }: Props) => {
             className="text-3xl"
           />
         </button>
+      </div>
+      <div className="px-8">
+        <div className="w-full flex justify-between text-2xl text-white rounded-xl">
+          <button
+            onClick={() => changeSearchBy("Ingredient")}
+            className={`border-2 border-white rounded-xl px-4 py-2 ${
+              searchBy === "Ingredient" ? "bg-white text-mainGreen" : ""
+            }`}
+          >
+            Ingredient
+          </button>
+          <button
+            onClick={() => changeSearchBy("Multi-Ingredient")}
+            className={`border-2 border-white rounded-xl px-4 py-2 ${
+              searchBy === "Multi-Ingredient" ? "bg-white text-mainGreen" : ""
+            }`}
+          >
+            Multi-Ingredient
+          </button>
+          <button
+            onClick={() => changeSearchBy("Dish Name")}
+            className={`border-2 border-white rounded-xl px-4 py-2 ${
+              searchBy === "Dish Name" ? "bg-white text-mainGreen" : ""
+            }`}
+          >
+            Dish Name
+          </button>
+        </div>
+        <div className="relative mt-4">
+          <input
+            placeholder={getInputPlaceholder()}
+            className="w-full p-2 pr-8 bg-mainGreen border-2 border-white rounded-xl placeholder:text-white focus:outline-none"
+          ></input>
+          <button className="absolute right-2 top-1/2 -translate-y-1/2 text-2xl">
+            <AiOutlineSearch />
+          </button>
+        </div>
       </div>
     </nav>
   );
