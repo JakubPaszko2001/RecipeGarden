02c3428afb39eeb60b1771a25d4c0b2fe1713495
JakubPaszko2001 <jakubpaszko01@gmail.com>
2023-06-01 23:33:58 +0200

    feat :sparkles: Add two fucntions fetchByIngredient and fetchByDishName

--- src/config/axios.tsx
+++ src/config/axios.tsx
@@ -88,4 +88,57 @@ const fetchDish = (dishId: string | undefined) => {
     });
 };
 
-export { fetchCategory, fetchPopular, fetchSpecificCategory, fetchDish };
+const fetchByIngredient = (Ingredient: string | undefined) => {
+  const options = {
+    method: "GET",
+    url: "https://themealdb.p.rapidapi.com/filter.php",
+    params: {
+      i: { Ingredient },
+    },
+    headers: {
+      "X-RapidAPI-Key": "57607c4060msh8305edef8d177d9p1a088fjsnad865a1577b2",
+      "X-RapidAPI-Host": "themealdb.p.rapidapi.com",
+    },
+  };
+
+  return axios
+    .request(options)
+    .then(function (response) {
+      // console.log(response.data.meals);
+      return response.data.meals;
+    })
+    .catch(function (error) {
+      // console.error(error);
+    });
+};
+
+const fetchByDishName = (dishName: string | undefined) => {
+  const options = {
+    method: "GET",
+    url: "https://themealdb.p.rapidapi.com/search.php",
+    params: { dishName },
+    headers: {
+      "X-RapidAPI-Key": "57607c4060msh8305edef8d177d9p1a088fjsnad865a1577b2",
+      "X-RapidAPI-Host": "themealdb.p.rapidapi.com",
+    },
+  };
+
+  return axios
+    .request(options)
+    .then(function (response) {
+      // console.log(response.data.meals);
+      return response.data.meals;
+    })
+    .catch(function (error) {
+      // console.error(error);
+    });
+};
+
+export {
+  fetchCategory,
+  fetchPopular,
+  fetchSpecificCategory,
+  fetchDish,
+  fetchByIngredient,
+  fetchByDishName,
+};
