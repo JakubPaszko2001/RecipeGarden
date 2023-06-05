495f76fcb2dc62cec8188db76b02a13a9e82ed1f
JakubPaszko2001 <jakubpaszko01@gmail.com>
2023-06-05 23:04:20 +0200

    Add search list and load more button

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineSearch } from "react-icons/ai";
+import { fetchByDishName, fetchByIngredient } from "../config/axios";
 
 interface Props {
   searchBar: boolean;
   setSearchBar: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+interface MealData {
+  idMeal: string;
+  strMeal: string;
+  strMealThumb: string;
+}
+
 const SearchBar = ({ searchBar, setSearchBar }: Props) => {
   const [searchBy, setSearchBy] = useState("Ingredient");
   const [inputValue, setInputValue] = useState("");
-
-  function changeSearchBy(value: string) {
-    setSearchBy(value);
-  }
+  const [fetchData, setFetchData] = useState<MealData[]>([]);
+  const [visibleItems, setVisibleItems] = useState(5);
 
   function getInputPlaceholder() {
     if (searchBy === "Ingredient") {
@@ -27,7 +32,25 @@ const SearchBar = ({ searchBar, setSearchBar }: Props) => {
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setInputValue(e.target.value);
-    console.log(inputValue);
+  }
+
+  async function handleSearch() {
+    try {
+      if (searchBy === "Ingredient" || searchBy === "Multi-Ingredient") {
+        const response = await fetchByIngredient(inputValue);
+        setFetchData(response);
+        console.log(response);
+      } else if (searchBy === "Dish Name") {
+        const response = await fetchByDishName(inputValue);
+        setFetchData(response);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function handleLoadMore() {
+    setVisibleItems((prevVisibleItems) => prevVisibleItems + 5);
   }
 
   return (
@@ -47,7 +70,7 @@ const SearchBar = ({ searchBar, setSearchBar }: Props) => {
       <div className="px-8">
         <div className="w-full flex justify-between text-2xl text-white rounded-xl">
           <button
-            onClick={() => changeSearchBy("Ingredient")}
+            onClick={() => setSearchBy("Ingredient")}
             className={`border-2 border-white rounded-xl px-4 py-2 ${
               searchBy === "Ingredient" ? "bg-white text-mainGreen" : ""
             }`}
@@ -55,7 +78,7 @@ const SearchBar = ({ searchBar, setSearchBar }: Props) => {
             Ingredient
           </button>
           <button
-            onClick={() => changeSearchBy("Multi-Ingredient")}
+            onClick={() => setSearchBy("Multi-Ingredient")}
             className={`border-2 border-white rounded-xl px-4 py-2 ${
               searchBy === "Multi-Ingredient" ? "bg-white text-mainGreen" : ""
             }`}
@@ -63,7 +86,7 @@ const SearchBar = ({ searchBar, setSearchBar }: Props) => {
             Multi-Ingredient
           </button>
           <button
-            onClick={() => changeSearchBy("Dish Name")}
+            onClick={() => setSearchBy("Dish Name")}
             className={`border-2 border-white rounded-xl px-4 py-2 ${
               searchBy === "Dish Name" ? "bg-white text-mainGreen" : ""
             }`}
@@ -76,10 +99,39 @@ const SearchBar = ({ searchBar, setSearchBar }: Props) => {
             placeholder={getInputPlaceholder()}
             onChange={handleChange}
             className="w-full p-2 pr-8 bg-mainGreen border-2 border-white rounded-xl placeholder:text-white focus:outline-none"
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                handleSearch();
+              }
+            }}
           ></input>
-          <button className="absolute right-2 top-1/2 -translate-y-1/2 text-2xl">
+          <button
+            className="absolute right-2 top-1/2 -translate-y-1/2 text-2xl"
+            onClick={handleSearch}
+          >
             <AiOutlineSearch />
           </button>
+        </div>
+        <div className="mt-4">
+          <ul className="h-[60vh] overflow-y-auto">
+            {fetchData.slice(0, visibleItems).map((meal) => (
+              <li
+                key={meal.idMeal}
+                className="w-full p-2 border-2 border-white rounded-xl"
+              >
+                <img src={meal.strMealThumb} alt="" />
+                <p>{meal.strMeal}</p>
+              </li>
+            ))}
+          </ul>
+          {visibleItems < fetchData.length && (
+            <button
+              className="mt-4 bg-white text-mainGreen rounded-xl px-4 py-2"
+              onClick={handleLoadMore}
+            >
+              Load More
+            </button>
+          )}
         </div>
       </div>
     </nav>
